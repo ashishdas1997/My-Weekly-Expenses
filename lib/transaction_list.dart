aff7ef605d3af6170a8dd99cab08138e3ad85a3d
Ashish <ashishdas.1997@gmail.com>
2020-05-06 17:10:45 +0530

    Making the list scrollable

--- lib/transaction_list.dart
+++ lib/transaction_list.dart
@@ -12,47 +12,45 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-            children: transaction.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.teal, width: 2)),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        ('\$${tx.amount}'),
+      child: ListView(
+          children: transaction.map((tx) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.teal, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      ('\$${tx.amount}'),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.teal),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.teal),
+                            color: Colors.black,
+                            fontSize: 20),
+                        textAlign: TextAlign.left,
                       ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                              fontSize: 20),
-                          textAlign: TextAlign.left,
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(color: Colors.grey),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList()
-        ),
+                      Text(
+                        DateFormat.yMMMd().format(tx.date),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          }).toList()
       ),
     );
   }
